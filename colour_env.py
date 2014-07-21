df0d1232024c02fd51e9125f007aa9584d05ed4f
SomethingCM <474784635@qq.com>
2014-07-21 13:41:20 +0800

    test
    test

---
+++ colour_env.py
@@ -0,0 +1,12 @@
+#这些是定义颜色的函数
+def ingreen(s):
+    return"%s[30;32;2m%s%s[0m"%(chr(27), s, chr(27))
+def indgreen(s):
+    return"%s[30;35;2m%s%s[0m"%(chr(27), s, chr(27))
+def inyellow(s):
+    return"%s[30;33;2m%s%s[0m"%(chr(27), s, chr(27))
+def inred(s):
+    return"%s[30;31;2m%s%s[0m"%(chr(27), s, chr(27))
+	
+	
+	
